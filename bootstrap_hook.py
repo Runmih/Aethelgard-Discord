ba0cb95a8b3c1680bba8187428813d9f90eec144
Runmih <runmih01@gmail.com>
2026-09-04 17:43:47 +0200

    Use button instructions in event panel

--- bootstrap_hook.py
+++ bootstrap_hook.py
@@ -31,12 +31,14 @@ def install_bootstrap() -> None:
             from crime_system import install as install_crime_system
             from interface_cleanup import install as install_interface_cleanup
             from command_consolidation import install as install_command_consolidation
+            from button_text_cleanup import install as install_button_text_cleanup
 
             install(main)
             install_expedition_system(main)
             install_crime_system(main)
             install_interface_cleanup(main)
             install_command_consolidation(main)
-        return _ORIGINAL_RUN(self, *args, **kwargs)
+            install_button_text_cleanup(main)
+        return _ORIGINAL_RUN(self, *args: Any, **kwargs: Any)
 
     commands.Bot.run = run_with_aethelgard_systems
